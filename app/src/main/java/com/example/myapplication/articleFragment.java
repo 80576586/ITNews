3f3954ccde3fe27d1d00feb9f1c6594132334beb
80576586 <13480576586@163.com>
2021-12-25 12:58:55 +0800

    修改

--- app/src/main/java/com/example/myapplication/articleFragment.java
+++ app/src/main/java/com/example/myapplication/articleFragment.java
@@ -43,10 +43,10 @@ public class articleFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_article, container, false);
 
     }
+
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
